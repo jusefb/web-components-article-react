74b4a19b1369c118d1156a2a37ae98ef656f220d
Jusef Belkasemi <jusef@in-fact.com>
2019-08-27 14:44:05 +0100

    Further configuration for react web component

---
+++ build/precache-manifest.e780afc4e675e35a64378684fc3f1c2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f1997a3679036be81a8dd8d5d93d8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01360fdd7ea241f5bc26",
+    "url": "/static/css/main.22170ed8.chunk.css"
+  },
+  {
+    "revision": "5218957344632947dde1",
+    "url": "/static/js/2.b05d3631.chunk.js"
+  },
+  {
+    "revision": "01360fdd7ea241f5bc26",
+    "url": "/static/js/main.15524ff7.chunk.js"
+  },
+  {
+    "revision": "70b6a27b4f4f24760839",
+    "url": "/static/js/runtime~main.9ee114c9.js"
+  }
+]);
